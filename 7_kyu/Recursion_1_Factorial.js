f88bb1dcbb295da24725366f12f8d3be1e38f66f
DmitryPatskalev <dimanord@yandex.ru>
2022-11-30 18:47:22 +0300

    commit

---
+++ 7_kyu/Recursion_1_Factorial.js
@@ -0,0 +1,8 @@
+const factorial = n => {
+  return n <= 1 ? 1 : n * factorial(n - 1)
+};
+
+console.log(factorial(0))//, 1);
+console.log(factorial(1))//, 1);
+console.log(factorial(2))//, 2);
+console.log(factorial(3))//, 6);
