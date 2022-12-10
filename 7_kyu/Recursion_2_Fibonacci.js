5f37e96ee82118d11b829ea8f7f7e1115e813eb5
DmitryPatskalev <dimanord@yandex.ru>
2022-12-10 15:21:14 +0300

    commit

---
+++ 7_kyu/Recursion_2_Fibonacci.js
@@ -0,0 +1,8 @@
+const fibonacci = n => {
+  return n === 1 || n === 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
+};
+
+console.log(fibonacci(1))//, 1);
+console.log(fibonacci(2))//, 1);
+console.log(fibonacci(3))//, 2);
+console.log(fibonacci(4))//, 3);
