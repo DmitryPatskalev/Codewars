54643a9c9a66984b8085ef3220a736b44ccc7bf1
DmitryPatskalev <dimanord@yandex.ru>
2023-03-02 09:15:34 +0300

    commit

---
+++ 7_kyu/Multiply_Word_in_String.js
@@ -0,0 +1,15 @@
+function modifyMultiply(str, loc, num) {
+    let emptyStr = ''
+    let symbol = '-'
+    const newStr = str.split(' ').splice(loc, 1).join('')
+    for (let i = 0; i < num; i++) {
+        emptyStr += newStr + symbol
+    }
+    return emptyStr.slice(0, emptyStr.length - 1)
+}
+
+console.log(modifyMultiply("This is a string", 3, 5))//, "string-string-string-string-string", "The string is incorrect");
+console.log(modifyMultiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.", 8, 10))//, "that-that-that-that-that-that-that-that-that-that");
+console.log(modifyMultiply("Self-control means wanting to be effective at some random point in the infinite radiations of my spiritual existence", 1, 1))//, "means");
+console.log(modifyMultiply("Is sloppiness in code caused by ignorance or apathy? I don't know and I don't care.", 6, 8))//, "ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance-ignorance");
+console.log(modifyMultiply("Everything happening around me is very random. I am enjoying the phase, as the journey is far more enjoyable than the destination.", 2, 5))//, "around-around-around-around-around")
